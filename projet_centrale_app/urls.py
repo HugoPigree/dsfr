1389a168f77ffdcda1ca3592bd77138d7d600530
Dev <h.pigreepro@gmail.com>
2024-12-04 11:09:46 +0100

    dico complet

--- projet_centrale_app/urls.py
+++ projet_centrale_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),  # Page d'accueil
+    path('', views.index, name='index'),
     path('<str:categorie_slug>/<str:projet_slug>/info/', views.info_projet, name='info_projet'),  # Page d'info
     path('<str:categorie_slug>/<str:projet_slug>/', views.projet_detail, name='projet_detail'),  # Projet
     path('<str:categorie_slug>/', views.categorie_detail, name='categories_projets'),  # Catégorie
