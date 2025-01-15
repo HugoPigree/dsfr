eed5f7a52ce93872bb9af790c37929df99b79def
Dev <h.pigreepro@gmail.com>
2025-01-15 10:43:41 +0100

    installation tailwinds

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './projet_centrale_app/templates/**/*.html',
+    './projet_centrale_app/static/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
